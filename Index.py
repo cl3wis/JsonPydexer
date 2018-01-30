1d18528c7f249f67f4e016cfa3f867676acece11
Chris Bailey <christianbailey0556@gmail.com>
2018-01-29 20:46:28 -0600

    Adds Index class, Index.add() operator, a pseudo-integration test, and updates old unit tests

--- Index.py
+++ Index.py
@@ -1,7 +1,60 @@
+import time
+import json
+from functools import reduce
+from operator import getitem
+from pathlib import Path
+
 class Index:
-    def __init__(self, filename=None):
-        if filename is None:
-            print("making a new index")
-        else:
-            print("opening an old index")
+    def __init__(self, key_names, rootPath, filename=None):
+        self.files = dict()
+        self.unique_indices = { key_name: {} for key_name in key_names }
+        self.group_indices = dict()
+        self.rootPath = rootPath
+
+
+    def add(self, filename):
+        with open(str(Path(self.rootPath, filename)), "r") as f:
+            f = json.load(f)
+
+            # update the filelist so we know we have handled this file at this time
+            self.files[filename] = int(time.time())
+
+            # check each of the unique_indices key names
+            key_names = list(self.unique_indices)
+            for key_name in key_names:
+                # get the value in the file that we will index on
+                key = reduce(getitem, key_name, f)
+
+                # if we don't already have a filename for this key, add this filename 
+                if not self.unique_indices.get(key_name).get(key, False):
+                    self.unique_indices[key_name][key] = filename
+                # else we already had a filename for this key, the key_name is no longer
+                # a unique identifier. move it to group_indices. we don't need to add the
+                # new file yet, since adding nonunique (group) indexes happens after this
+                else:
+                    self.move_unique_to_group(key_name)
+
+            # check each of the group_indices key names
+            for key_name in self.group_indices:
+                # get the value in the file that we will index on
+                key = reduce(getitem, key_name, f)
+
+                # if we have already added the key to the group_keys[keyname], just
+                # add our filename to the set for that key. otherwise, we must add
+                # key to group_keys[keyname]
+                if self.group_indices[key_name].get(key, False):
+                    self.group_indices[key_name][key].add(filename)
+                else:
+                    self.group_indices[key_name][key] = set([filename])
+
+
+    def move_unique_to_group(self, key_name):
+        # create the new key_name in group_indices
+        self.group_indices[key_name] = dict()
+
+        # add each key,value pair in the unique key_name to the group key_name
+        for key, value in self.unique_indices[key_name].items():
+            self.group_indices[key_name][key] = set([value])
+        # delete the key_name from the unique key_names
+        del self.unique_indices[key_name]
 
--- JsonPydexer.py
+++ JsonPydexer.py
@@ -1,11 +1,14 @@
 __author__ = "Christian Bailey (me@christianbailey.me)"
-__version__ = "0.2.2"
+# TODO update version
+__version__ = "x.x.x"
 
 import os
 import json
 import pickle
 import sys
 import stat
+from Index import Index
+from pathlib import Path
 from functools import reduce
 from operator import getitem
 
@@ -28,29 +31,8 @@ class JsonPydexer:
         elif not (os.access(self.rootPath, os.W_OK)):
             raise ValueError("Specified directory is not writable")
 
-    def give_me_indexes(r, key, self):
-        index = dict()
 
-        for root, subFolders, files in os.walk(self.rootPath):
-            for file in files:
-                name, extension = os.path.splitext(file)
-                if extension == ".json":
-                    with open(root + "/" + file) as f:
-                        j = json.load(f)
-                        try:
-                            file_key = reduce(getitem, key, j)
-                            if r:
-                                index[file_key] = os.path.join(
-                                    os.path.relpath(root, start=self.rootPath), file
-                                )
-                            else:
-                                index[file_key] = file
-                        except KeyError:
-                            continue
-
-        return index
-
-    def index(self, key, r=False, filename=None):
+    def index(self, keys, r=True):
         """Index .json files in the root path. Pickles the index as a dict, with
         whichever key you specify as the key and relative filenames as values to
         save on storage space.
@@ -60,20 +42,44 @@ class JsonPydexer:
             nested key
             r (bool, optional): recurse into lower directories or not. not yet
             implemented
-            filename (string, optional): Filename to save the index as. defaulti
-            is str(key) + ".pickle"
         Returns: None
         """
-        if type(key) is str:
-            key = [key]
+        if type(keys) is str:
+            raise ValueError("keys argument must be a list of strings or iterables yielding strings")
+        keys = [tuple([key]) if type(key) is str else tuple(key) for key in keys]
 
-        if filename is None:
-            filename = ''.join(key + [".pickle"])
+        if os.path.isfile(os.path.join(self.rootPath, ".jp.pkl")):
+            # we will use update methods
+            index = self.load()
+        else:
+            # we will use create methods
+            index = Index(keys, self.rootPath)
 
-        if os.path.isfile(filename):
-            raise ValueError("index file alreay exists")
+        # load filenames in I as set_I
+        set_I = set(index.files)
 
-        index = JsonPydexer.give_me_indexes(r, key, self)
+        # load filenames in rootpath as set_F
+        p = Path(self.rootPath)
+        files = p.glob("**/*.json")
+        set_F = set([str(f.relative_to(self.rootPath)) for f in files])
 
-        with open(filename, mode="wb") as f:
+        # remove zombies (remove set(set_I - set_F) from index and set_I
+        zombies = set_I - set_F
+        for zombie in zombies:
+            index.remove(zombie)
+            set_I.remove(zombie)
+
+        #TODO update modified files
+        # for i in set_I
+            # if (index -> files -> I -> timestamp) < (rootpath -> I -> timestamp)
+                # index.update(I)
+                # remove I from set_I
+
+        # add new files from set_F (eg set_F - set_I) to I
+        newfiles = set_F - set_I
+        for newfile in newfiles:
+            index.add(newfile)
+
+        with open(".jp.pkl", mode="wb") as f:
             pickle.dump(index, f)
+
