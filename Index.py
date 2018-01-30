de054793bd0dbc6095d29ab9e90ed25b30162312
Chris Bailey <christianbailey0556@gmail.com>
2018-01-29 20:45:38 -0600

    adds new Index class and the Index.add() method, plus tests

---
+++ Index.py
@@ -0,0 +1,7 @@
+class Index:
+    def __init__(self, filename=None):
+        if filename is None:
+            print("making a new index")
+        else:
+            print("opening an old index")
+
